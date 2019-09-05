23b68bbe5c4a7866a7e84919fbe7ed93aba24f53
George An <carbongo@hotmail.com>
2019-09-05 10:35:33 +0900

    Added comments

--- src/scripts/master.js
+++ src/scripts/master.js
@@ -1,5 +1,10 @@
+// Import your script files
+// import interaction from './interaction.js';
+
+// Import your styles
 import '../styles/master.less';
 
+// Your script goes here
 const main = () => {
     return "Hello, world!"
 }
